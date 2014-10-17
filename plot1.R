1a243e5d14479cbfa5a29892f8c91c1421f26172
rennyatwork <rennyatwork@gmail.com>
2014-10-16 22:28:48 -0400

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,23 @@
+initialize <-function ()
+{
+  library(sqldf)
+}
+
+##This function return the full path+ file name to be read
+getFullPathFileName <-function(pFileName)
+{
+  return (paste(paste(getwd(),paste("exdata_data_NEI_data", pFileName, sep="/"), sep="/"), ".rds" , sep=""))
+}
+
+
+## From http://stackoverflow.com/questions/14923581/open-files-saved-with-rs-saverds-serialization-interface-with-stata
+#resave <- function(infile)
+#{
+#  x <- readRDS(infile)
+#  outfile <- sub("\\.rds$", "\\.csv", infile)
+#  write.csv(x, outfile)
+#}
+
+
+##This function loads the desired dataset (NEI or SCC)
+get
