b59fe2d7803558a8b4930ef890cf3accc3d8d8e1
rennyatwork <rennyatwork@gmail.com>
2014-10-18 11:36:30 -0400

    plot1 file and image

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+##initialize to use library sqldf
 initialize <-function ()
 {
   library(sqldf)
@@ -16,20 +17,30 @@ getDt <- function()
   #transform(NEI, year = factor(year)
 }
 
-sumPerYear <-function()
+## saves and close the file
+saveAndClose <- function(pName)
 {
-  
+  dev.copy(png, paste(pName, ".png", sep=""), width=480, height=480)
+  dev.off()
 }
 
-## From http://stackoverflow.com/questions/14923581/open-files-saved-with-rs-saverds-serialization-interface-with-stata
-#resave <- function(infile)
-#{
-#  x <- readRDS(infile)
-#  outfile <- sub("\\.rds$", "\\.csv", infile)
-#  write.csv(x, outfile)
-#}
-
-
+plot1<-function()
+{
+  #initialize
+  initialize()
+  
+  #read the data into variable NEI
+  NEI<-getDt()
+  
+  #generate new datatable with data suitable for plotting
+  dt2 <-  sqldf("select sum(Emissions), year from NEI group by year")
+  
+  #plots the graph
+  plot(data.frame(dt2[2], dt2[1]/1000.00), type='l', xlab='year', ylab=' Emissions PM25 (x 1000)', main='Total Emissions PM25 (x 1000) per year')
+  
+  #saves and closes
+  saveAndClose("plot1")
+}
 ##This function loads the desired dataset (NEI or SCC)
 #get
 
@@ -37,4 +48,4 @@ sumPerYear <-function()
 # NEI <- readRDS(getFullPathFileName('summarySCC_PM25'))
 #dt2 <-  sqldf("select sum(Emissions), year from NEI group by year")
 #plot(data.frame(dt2[2], dt2[1]))
-# plot(data.frame(dt2[2], dt2[1]/1000.00), type='l', xlab='year', ylab=' Emissions (x 1000)', main='Total Emissions (x 1000) per year')
+# plot(data.frame(dt2[2], dt2[1]/1000.00), type='l', xlab='year', ylab=' Emissions PM25 (x 1000)', main='Total Emissions PM25 (x 1000) per year')
